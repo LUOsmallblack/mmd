36af018d439f4106800ef094d828afb43e241744
WalkerNJU <walkernju@gmail.com>
2015-02-17 00:40:45 +0800

    use ng-repeat to show a list of songs(unfinished)

--- player.js
+++ player.js
@@ -7,4 +7,10 @@ angular.module("ngAppPlayer", [])
     $scope.current.bufferstyle = {'width':'80%'};
     $scope.current.currentstyle = {'width':'30%'};
     $scope.current.handlestyle = {'left':'30%'};
+
+    $scope.musiclist = [
+    	{ id: 0, name: "Love&Loyalty", author: "Juji Gu", duration: "3:45"},
+    	{ id: 1, name: "If I were a Boy", author: "Beyonce" ,duration: "4:12"},
+    	{ id: 2, name: "Dragon Knight", author: "Jay Chou", duration: "5:07"}
+    ];
   }]);
