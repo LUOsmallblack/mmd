cf2f6938427b1a33c2ee532a05682fd6af6195ea
Clouds <Clouds.Flowing@gmail.com>
2015-02-16 21:37:41 +0800

    add progress bar in player

--- player.js
+++ player.js
@@ -1,7 +1,10 @@
 angular.module("ngAppPlayer", [])
 .controller("MusicListController", ["$scope", function MusicListController($scope) {
     $scope.current = {}
-    $scope.current.title = "tit";
-    $scope.current.artist = "art";
-    $scope.current.album = "alb";
+    $scope.current.title = "Title";
+    $scope.current.artist = "Artist";
+    $scope.current.album = "Album";
+    $scope.current.bufferstyle = {'width':'80%'};
+    $scope.current.currentstyle = {'width':'30%'};
+    $scope.current.handlestyle = {'left':'30%'};
   }]);
