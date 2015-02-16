7af874cf47b35b5ce578f8c6bcff5f2dc6c2c3a0
Clouds <Clouds.Flowing@gmail.com>
2015-02-17 02:16:52 +0800

    add musiclist to player

--- player.js
+++ player.js
@@ -1,16 +1,18 @@
 angular.module("ngAppPlayer", [])
 .controller("MusicListController", ["$scope", function MusicListController($scope) {
-    $scope.current = {}
-    $scope.current.title = "Title";
-    $scope.current.artist = "Artist";
-    $scope.current.album = "Album";
-    $scope.current.bufferstyle = {'width':'80%'};
-    $scope.current.currentstyle = {'width':'30%'};
-    $scope.current.handlestyle = {'left':'30%'};
+  $scope.current = {}
+  $scope.current.title = "Title";
+  $scope.current.artist = "Artist";
+  $scope.current.album = "Album";
+  $scope.current.time = "00:00";
+  $scope.current.duration = "04:11";
+  $scope.current.bufferstyle = {'width':'100%'};
+  $scope.current.currentstyle = {'width':'30%'};
+  $scope.current.handlestyle = {'left':'30%'};
 
-    $scope.musiclist = [
-    	{ id: 0, name: "Love&Loyalty", author: "Juji Gu", duration: "3:45"},
-    	{ id: 1, name: "If I were a Boy", author: "Beyonce" ,duration: "4:12"},
-    	{ id: 2, name: "Dragon Knight", author: "Jay Chou", duration: "5:07"}
-    ];
-  }]);
+  $scope.musiclist = [
+    { id: 0, title: "Love&Loyalty", author: "Juji Gu", duration: "3:45"},
+    { id: 1, title: "If I were a Boy", author: "Beyonce" ,duration: "4:12"},
+    { id: 2, title: "Dragon Knight", author: "Jay Chou", duration: "5:07"}
+  ];
+}]);
